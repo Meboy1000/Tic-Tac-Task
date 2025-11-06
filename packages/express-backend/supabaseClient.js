55b4f63d10a2ce98ff3109ceaf23884af338f70b
JavierMB03 <x.medinabueno@gmail.com>
2025-11-06 14:06:15 -0800

    starting new branch

--- packages/express-backend/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseurl = process.env.SUPABASE_URL
-const supabasekey = process.env.SUPABASE_KEY
-
-const supabase = createClient(supabaseurl, supabasekey)
-
-export default supabase
