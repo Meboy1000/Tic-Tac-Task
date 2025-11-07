c7f606ed2e7783ef265eb001cc057c82074b42b1
MatthewPhan <Matthewminhphan@gmail.com>
2025-11-06 17:31:06 -0800

    css

--- packages/react-frontend/src/components/landingPage.tsx
+++ packages/react-frontend/src/components/landingPage.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Landing } from "../application/landingpageData";
-import styles from "./landingpage.css";
+import styles from "./landingpage.module.css";
 
 export default function LandingPagePreview(props: Landing) {
 	return (
+	<>
 		<div>	
-			<b> <h3> {props.title} </h3> </b>
+			<b> <h3 className={styles.title}> {props.title} </h3> </b>
 		</div>
+		<div className={styles.loginContainer}>
+			<div className={styles.login}> 
+				<h3> {props.username} </h3>
+				<h3> {props.password} </h3>
+			</div>
+		</div>
+	</>
 	);
 }
