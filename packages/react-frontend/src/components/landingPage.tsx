a9dfc0a7c541b18172c792fc29108f11e305e649
MatthewPhan <Matthewminhphan@gmail.com>
2025-11-07 12:52:36 -0800

    latest css

--- packages/react-frontend/src/components/landingPage.tsx
+++ packages/react-frontend/src/components/landingPage.tsx
@@ -3,17 +3,45 @@ import { Landing } from "../application/landingpageData";
 import styles from "./landingpage.module.css";
 
 export default function LandingPagePreview(props: Landing) {
-	return (
-	<>
-		<div>	
-			<b> <h3 className={styles.title}> {props.title} </h3> </b>
-		</div>
-		<div className={styles.loginContainer}>
-			<div className={styles.login}> 
-				<h3> {props.username} </h3>
-				<h3> {props.password} </h3>
-			</div>
-		</div>
-	</>
-	);
+  return (
+    <>
+      <div>
+        <h3 className={styles.title}>{props.title}</h3>
+      </div>
+
+      <div className={styles.loginContainer}>
+        <form
+          className={styles.login}
+          onSubmit={(e) => {
+            e.preventDefault(); // remove this later when you handle real submit
+            console.log("Username:", (e.currentTarget.elements.namedItem("username") as HTMLInputElement).value);
+            console.log("Password:", (e.currentTarget.elements.namedItem("password") as HTMLInputElement).value);
+          }}
+        >
+          <div className={styles.field}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              defaultValue={props.username}
+            />
+          </div>
+
+          <div className={styles.field}>
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              defaultValue={props.password}
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </>
+  );
 }
+
