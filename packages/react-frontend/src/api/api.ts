361ba4a258ca0cc3a71ff013fcfd22fc0d35cc7c
Willwrum <williamqpierce@gmail.com>
2025-11-18 20:16:27 -0800

    Added exportable functions to react-frontend/src/api, requires API_URL in .env to not just use localhost In order to use them in the frontend, will need to import like from the other modules

---
+++ packages/react-frontend/src/api/api.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = import.meta.env.API_URL || "http://localhost:8000";
+
+export default BASE_URL;
---
+++ packages/react-frontend/src/api/user.ts
@@ -0,0 +1,28 @@
+import BASE_URL from "../api/api";
+
+export async function fetchUsers() {
+  const res = await fetch(`${BASE_URL}/users`);
+  return res.json();
+}
+
+export async function fetchUserById(id: string) {
+  const res = await fetch(`${BASE_URL}/users/${id}`);
+  if (!res.ok) throw new Error("User not found");
+  return res.json();
+}
+
+export async function createUser(user: any) {
+  const res = await fetch(`${BASE_URL}/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  });
+  if (!res.ok) throw new Error("Error saving user");
+  return res.json();
+}
+
+export async function deleteUserById(id: string) {
+  const res = await fetch(`${BASE_URL}/users/${id}`, { method: "DELETE" });
+  if (!res.ok) throw new Error("User not found");
+  return true;
+}
