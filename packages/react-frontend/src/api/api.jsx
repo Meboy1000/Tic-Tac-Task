de60b71cc2c0dffc3d7f62fea34110c017834815
Willwrum <williamqpierce@gmail.com>
2025-12-04 11:07:49 -0800

    Revert to old api url fix??

--- packages/react-frontend/src/api/api.jsx
+++ packages/react-frontend/src/api/api.jsx
@@ -1,5 +1,3 @@
-//export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
-export const BASE_URL = "https://yellow-mud-0062e1810.3.azurestaticapps.net/" || "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export default BASE_URL;
