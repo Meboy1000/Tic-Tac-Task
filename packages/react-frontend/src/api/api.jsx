fa8520ec9b6fef63466612a23a39c75a78d8871b
Cody Barriger <66487647+Meboy1000@users.noreply.github.com>
2025-12-07 13:57:27 -0800

    undid last commit
    it was brokey

--- packages/react-frontend/src/api/api.jsx
+++ packages/react-frontend/src/api/api.jsx
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL || "https://tictactask-g3c4brhnf8ahayau.westus3-01.azurewebsites.net";
 
 export default BASE_URL;
